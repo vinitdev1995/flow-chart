e9c9929d450a27ced83198ca2e47e4ea34b637f6
vinitdevani <devanivinitkumar@gmail.com>
2021-08-13 16:05:48 +0530

    library css changes

--- src/Component/Library/ListOption.js
+++ src/Component/Library/ListOption.js
@@ -3,5 +3,9 @@ export const option = [
     {title:"Logic and Triggers"},
     {title:"Subscription"},
     {title:"Connect user to a human"},
-    {title:"Plugins"}
+    {title:"Plugins"},
+    {title:"Plugins"},
+    {title:"Plugins"},
+    {title:"Plugins"},
+    {title:"Plugins"},
 ]
