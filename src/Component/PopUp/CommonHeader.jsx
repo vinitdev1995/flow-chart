2f68ba5967d3cfd8a748927e7e59a2746102c013
vinitdev1995 <vin8469304602>
2021-08-13 00:27:56 -0700

    chnages

--- src/Component/PopUp/CommonHeader.jsx
+++ src/Component/PopUp/CommonHeader.jsx
@@ -1,56 +1,45 @@
 import React,{useEffect,useState} from "react"
 import { Modal, Button } from 'antd';
-import search from "../../assets/images/search.png"
-
-
+import { GlobalOutlined,SearchOutlined } from '@ant-design/icons';
+import { Input } from 'antd';
+import './CommonHeader.scss'
 
 
 const Commonheader = (props) => {
     const {tital,img} = props;
     useEffect(()=>{
-        // setVisible(true);
+        setVisible(true);
     },[])
     const [visible,setVisible] = useState(false);
+
     const showModal = () => {
         setVisible(true);
     }
     const handleOk = (e) => {
-        console.log(e);
         setVisible(false);
     }
     const handleCancel = (e) => {
-        console.log(e);
         setVisible(false);
     }
     return (
         <div className="CommonHeader">
             <h1 onClick={showModal}>hi</h1>
             <Modal
-                // title="Library"
                 visible={visible}
                 onOk={handleOk}
                 onCancel={handleCancel}
                 closable={false}
             >
-
-                {/*<div className="d-flex flex-row">*/}
-                {/*    <img src={img} alt="icon" className="d-flex align-items-center"/>*/}
-                {/*    <h1>{tital}</h1>*/}
-                {/*    <h1 className="d-inline mx-4">Library</h1>*/}
-                {/*</div>*/}
-                {/*<div>*/}
-                {/*    <a href="#">Drag and drop block in to your flow</a>*/}
-                {/*</div>*/}
-                {/*<div className="d-flex flex-row">*/}
-                {/*    <div>*/}
-                {/*        <input type="text"/>*/}
-                {/*    </div>*/}
-                {/*    <div className="search-icon">*/}
-                {/*        <img src={search}/>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-                <p>Some contents...</p>
-                <p>Some contents...</p>
+                <div className="library-heading">
+                    <GlobalOutlined />
+                    <div className="library1">
+                        <h4>Library</h4>
+                    </div>
+                </div>
+                <div>Drag and drop a block into  your flow</div>
+                <div className="Search-field">
+                    <Input suffix={<SearchOutlined />} />
+                </div>
             </Modal>
         </div>
 
